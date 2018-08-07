8fc9ecc680cc1686cb9fc39a49f9be26173801c5
lorio <lori@loriortiz.com>
2018-08-07 16:31:55 -0400

    Display game

--- arcadegame/js/app.js
+++ arcadegame/js/app.js
@@ -24,10 +24,20 @@ Enemy.prototype.render = function() {
 
 // Now write your own player class
 // This class requires an update(), render() and
-// a handleInput() method.
-const player = function(){
-    this.sprite = char-princess-girl.png;
+
+class Player {
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.sprite = 'images/char-princess-girl.png;'
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    //update
+    // a handleInput() method.
 }
+const player = new Player();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
